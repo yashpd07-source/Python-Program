f8e666b903c9197d6aa121939e8588150d6df307
yashpd07-source <yashpd07@gmail.com>
2026-02-17 20:29:09 -0500

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,10 @@
+Praveen = "Praveen:7/21/1977"
+print(Praveen)
+Madhavi = "Madhavi:1/9/1983"
+print(Madhavi)
+Saachi = "Saachi:6/21/2010"
+print(Saachi)
+Srihan = "Srihan:3/21/2014"
+print(Srihan)
+Zachery = "Zachery:3/6/2014"
+print(Zachery)
