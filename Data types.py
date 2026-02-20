e2138a970f1bf48c1ad2fcac52e0eb2383597ea7
yashpd07-source <yashpd07@gmail.com>
2026-02-19 19:27:03 -0500

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,13 @@
+a = 10
+print("type of a:",type(a))
+b = 6.7
+print("type of b:",type(b))
+c = "Happy"
+print("type of c:",type(c))
+d = True
+print("type of d:",type(d))
+print("AFTER TYPECASTING")
+b = int(b)
+print("type of b:",type(b))
+a = str(a)
+print("type of a:",type(a))
