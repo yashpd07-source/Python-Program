b68029cb22f199ceecf3ed541078a0ebc55efcd6
yashpd07-source <yashpd07@gmail.com>
2026-02-26 19:35:38 -0500

    Add files via upload

---
+++ Check greater.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number:"))
+if number > 15:
+    print("Number is greater than 15")
+    print("I'm an if block!")
+else:
+    print("Number is less than 15")
+    print("I'm an else block!")
