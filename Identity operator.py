7aea32ac9fe49dd327de8e9ae5c4091e5c50876b
yashpd07-source <yashpd07@gmail.com>
2026-03-05 19:39:56 -0500

    Add files via upload

---
+++ Identity operator.py
@@ -0,0 +1,15 @@
+x = 5
+if (type(x) is int):
+    print("True")
+else:
+    print("False")
+x = 5.5
+if (type(x) is not float):
+    print("True")
+else:
+    print("False")
+x = 20 
+y = 20
+y = 30
+if (x is not y):
+    print("x & y have DIFFERENT identity!")
