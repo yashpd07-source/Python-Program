2acc13b22a54ca26324c2bdc9ca0dc8f5b316761
yashpd07-source <yashpd07@gmail.com>
2026-03-24 19:55:28 -0400

    Add files via upload

---
+++ Prime number between a range.py
@@ -0,0 +1,10 @@
+lower = int(input("Enter a lower range: "))
+upper = int(input("Enter a upper range: "))
+print("Prime Numbers between", lower, "and", upper, "are:")
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):
+            if(num % i) == 0:
+                break
+        else:
+                print(num)
