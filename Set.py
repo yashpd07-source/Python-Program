bf56440fd943f3ea2c3aaf472ba26344c89495c8
yashpd07-source <yashpd07@gmail.com>
2026-05-19 19:30:04 -0400

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,13 @@
+my_set = {1, 2, 3}
+print(my_set)
+my_set = {1.0, "Hello", (1, 2, 3)}
+my_set = {1, 2, 3, 4, 3, 2}
+print(my_set)
+my_set = set([1, 2, 3, 2])
+print(my_set, "\n")
+num_set = set([0,1,3,4,5])
+print("Original set:")
+print(num_set)
+num_set.pop()
+print("After removing thr first element from the first set: ")
+print(num_set, "\n")
