025b8b5fc891ca4c0e2a7a7a403da322a54c084c
yashpd07-source <yashpd07@gmail.com>
2026-05-21 19:38:10 -0400

    Add files via upload

---
+++ Zip.py
@@ -0,0 +1,12 @@
+s1 = {2, 3, 1}
+s2 = {'b','a','c'}
+s3 = list(zip(s1,s2))
+print(s3,"\n")
+list1 = [1, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+for x, y in zip(list1, list2[::-1]):
+    print(x,y)
+stocks = ['relience', 'infosys', 'tcs']
+price = [2175, 1127, 2750]
+new_dict = {stocks: price for stocks, prices in zip(stocks, price)}
+print('\n{}'.format(new_dict))
