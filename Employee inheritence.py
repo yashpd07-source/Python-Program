fabb6963f8c6397e6c2f4663d013c23fc070fdc3
yashpd07-source <yashpd07@gmail.com>
2026-06-11 14:27:51 -0400

    Add files via upload

---
+++ Employee inheritence.py
@@ -0,0 +1,14 @@
+class Person( object):
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+class Employee(Person):
+    def __init__(self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
+        Person.__init__(self, name, idnumber)
+a = Employee('Yash', 67676747474, 2000000000000000000000000000000000, "CEO")
+a.display()
