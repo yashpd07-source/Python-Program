fefa85cc102e2ca8edbe68c75eb9be9b385836fb
yashpd07-source <yashpd07@gmail.com>
2026-06-12 13:46:12 -0400

    Add files via upload

---
+++ Keep it private.py
@@ -0,0 +1,9 @@
+class myClass:
+    __privateVar = 27
+    def __privMeth(self):
+        print("I'm inside class myClass")
+    def hello(self):
+        print("Private VAriable value: ", myClass.__privateVar)
+foo = myClass()
+foo.hello()
+foo.__privMeth
